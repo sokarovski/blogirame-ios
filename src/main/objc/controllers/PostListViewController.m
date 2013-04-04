da6ddf957c60c5e652260a3d98e8c2fdd6126b94
sokarovski <psokarovski@gmail.com>
2013-04-04 20:50:31 +0200

    afnetworking added, data fetched

--- src/main/objc/controllers/PostListViewController.m
+++ src/main/objc/controllers/PostListViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "PostListViewController.h"
+#import "AFNetworking.h"
 
-@interface PostListViewController ()
+@interface PostListViewController () {
+    AFJSONRequestOperation *operation;
+}
 
 @end
 
@@ -24,16 +27,66 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    [self fetchNewestPostsList];
     //start download of the posts if they are not downloaded
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [newestPosts count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"PostCell"];
+    }
+    NSDictionary *postEntry = [newestPosts objectAtIndex:indexPath.row];
+    [cell.textLabel setText:[postEntry valueForKey:@"title"]];
+    return cell;
 }
+
+#pragma mark Data Download
+
+- (void)fetchNewestPostsList
+{
+    NSString *url = @"http://blogirame.mk/jsonapi/main";
+    NSURL *base = [NSURL URLWithString:url];
+    NSURLRequest *req = [NSURLRequest requestWithURL:base];
+    
+    operation = [AFJSONRequestOperation
+                 JSONRequestOperationWithRequest:req
+                 success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                     newestPosts = nil;
+                     newestPosts = [JSON objectForKey:@"results"];
+                     [myTableView reloadData];
+                 }
+                 failure:^(NSURLRequest *request, NSURLResponse *response, NSError *error ,id JSON) {
+                     NSLog(@"%@",[error debugDescription]);
+                 }];
+    
+    [operation start];
+}
+
+- (void)fetchTopPostsList
+{
+    NSString *url = @"http://blogirame.mk/jsonapi/main/order/popular/";
+    NSURL *base = [NSURL URLWithString:url];
+    NSURLRequest *req = [NSURLRequest requestWithURL:base];
+    
+    operation = [AFJSONRequestOperation
+                 JSONRequestOperationWithRequest:req
+                 success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                     topPosts = nil;
+                     topPosts = [JSON objectForKey:@"results"];
+                     [myTableView reloadData];
+                 }
+                 failure:^(NSURLRequest *request, NSURLResponse *response, NSError *error ,id JSON) {
+                     NSLog(@"%@",[error debugDescription]);
+                 }];
+    
+    [operation start];
+}
+
 @end
